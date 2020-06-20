73554a19c793470276522f538af65613e9001f89
Vivek Singh <vivek.singh@coveiot.com>
2020-06-20 15:30:34 +0530

    Added sql script

---
+++ ecom-web/src/main/resources/init_db.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_name` varchar(45) NOT NULL,
+  `item_desc` varchar(45) DEFAULT NULL,
+  `quantity` int(11) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `price` decimal(7,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `price` decimal(7,2) NOT NULL,
+  `order_date` datetime NOT NULL,
+  `delivery_date` datetime DEFAULT NULL,
+  `courier_partner` varchar(45) DEFAULT NULL,
+  `created_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ ecom-web/src/main/resources/requirment.md
@@ -0,0 +1,8 @@
+```It should have the following operations as APIs```
+
+1.    CRUD operations on items
+2.    All items listing
+3.    Single & bulk ordering (Just consider the item, no. of items & email ids as params for ordering)
+4.    All orders
+
+Please consider all the cases like out of stock etc. while making the application. You can also add more features/APIs as suitable for you.
